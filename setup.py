27a73fc98d4cdcf2ac390dfe1cc7b3c194621af3
Argyrum <Fact_core@outlook.com>
2023-08-18 23:47:26 +0200

    Initial Changes

--- setup.py
+++ setup.py
@@ -11,14 +11,15 @@ setuptools.setup(
     description="Transfer files with Marlin 2.0 firmware using Marlin Binary Protocol Mark II",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/charleswillis3/marlin-binary-protocol",
+    url="https://github.com/Argyrum/marlin-binary-protocol",
     packages=setuptools.find_packages(),
-    install_requires=["heatshrink>=0.3.2", "heatshrink2>=0.9", "pyserial>=3.4", "backports.time_perf_counter; python_version < '3.3'"],
+    install_requires=[
+        "heatshrink2>=0.9",
+        "pyserial>=3.4"],
     classifiers=[
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=2.6, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
+    python_requires='>=3.5, <4',
 )
