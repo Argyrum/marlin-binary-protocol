88ba08840a1b1395186064184fba3fd9c0ab97f8
Argyrum <Fact_core@outlook.com>
2023-08-19 00:09:29 +0200

    Final changes

--- binproto2/transfer.py
+++ binproto2/transfer.py
@@ -18,15 +18,15 @@ class ConsoleLogger(object):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Send files over a serial port to Marlin')
-    parser.add_argument('source', help='source path')
-    parser.add_argument('destination', nargs='?', help='destination path')
-    parser.add_argument("-p", "--port", default="COM3", help="serial port to use")
-    parser.add_argument("-b", "--baud", default="115200", help="baud rate of serial connection")
-    parser.add_argument("-d", "--blocksize", default="512", help="defaults to autodetect")
-    parser.add_argument("-r", "--reset", action='store_true', help="Reset after transfer (firmware flash)")
-    parser.add_argument("-t", "--test", action='store_true', help="Benchmark the serial link without storing the file")
-    parser.add_argument("-c", "--compression", action='store_true', help="Enable compression")
-    parser.add_argument("-x", "--timeout", default="1000", help="Communication timout, lossy/slow connections need higher values")
+    parser.add_argument('source',                                       help='Source path')
+    parser.add_argument('destination',          nargs='?',              help='Destination path (default SD root folder)')
+    parser.add_argument("-p", "--port",         default="COM3",         help="Serial port to use (default COM3)")
+    parser.add_argument("-b", "--baud",         default="115200",       help="Baud rate of serial connection (default 115200)")
+    parser.add_argument("-d", "--blocksize",    default="512",          help="Defaults to autodetect (Default 512)")
+    parser.add_argument("-r", "--reset",        action='store_true',    help="Reset after transfer (For firmware flash, needed for E3V2s)")
+    parser.add_argument("-t", "--test",         action='store_true',    help="Benchmark the serial link without storing the file")
+    parser.add_argument("-c", "--compression",  action='store_true',    help="Enable compression (Recommended for files above 500kB)")
+    parser.add_argument("-x", "--timeout",      default="1000",         help="Communication timout, lossy/slow connections need higher values")
 
     args = parser.parse_args()
 
@@ -45,6 +45,7 @@ if __name__ == "__main__":
             protocol.send_ascii("M997")
         else:
             protocol.send_ascii("M21")
+            
     except KeyboardInterrupt:
         filetransfer.abort()
 
